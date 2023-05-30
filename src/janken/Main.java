32820e1f2d765b0ab5fae05153a003988ce723a6
kamishubaizumo <kamishubaizumo@yahoo.co.jp>
2023-05-30 12:54:12 +0900

    firstcommit

---
+++ src/janken/Main.java
@@ -0,0 +1,10 @@
+package janken;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ゲームWindowの生成と表示
+        MainWindow window = new MainWindow();
+        window.show();
+    }
+}
---
+++ src/janken/Status.java
@@ -0,0 +1,13 @@
+package janken;
+
+
+
+/**
+ * ゲームのプレイ状況
+ */
+public enum Status {
+  /** 待ち状態 */
+  Wait,
+  /** 結果発表中 */
+  Done
+}
